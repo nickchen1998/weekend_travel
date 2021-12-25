61488828caf54f720056711c0731ad272e6e302b
nick <nick1998chen0725@gmail.com>
2021-12-25 20:16:09 +0800

    create songshan spider

---
+++ crawlers/crawlers/spiders/songshan.py
@@ -0,0 +1,14 @@
+import scrapy
+from bs4 import BeautifulSoup
+from crawlers.items import ExhibitionItem
+from pprint import pprint
+
+
+class SongshanSpider(scrapy.Spider):
+    name = 'songshan'
+    allowed_domains = ['www.songshanculturalpark.org']
+    start_urls = ['http://www.songshanculturalpark.org/exhibitionlist.aspx']
+
+    def parse(self, response, **kwargs):
+        exhibition = ExhibitionItem()
+        print(response.text)
